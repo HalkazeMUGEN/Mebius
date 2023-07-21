fbdf9070854d62404a4b917ef39f6a2657c323f2
Halkaze <halkaze74e@gmail.com>
2023-07-22 02:21:19 +0900

    Compat main branch

--- share/Mebius.h
+++ share/Mebius.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <Windows.h>
 #include <filesystem>
 
 using namespace std;
--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,24 +1,6 @@
 ﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
-#include "framework.h"
-#include "Mebius.h"
+#include "_Mebius.h"
 #include <vector>
-
-// プロトタイプ宣言
-void Head(void);
-void ESCAPE_EAX(void);
-int __stdcall Tail(int eax);
-int findTargetHookByStart(void* target);
-int findTargetHookByReturn(void* target);
-
-// フックリスト
-struct HOOK
-{
-    void* targetStartAddr = NULL;
-    void* targetReturnAddr = NULL;
-    BYTE targetOrigBytes[5] = { 0xCC, 0xCC, 0xCC, 0xCC, 0xCC };
-    vector<void*> cbHeadFuncAddr;
-    vector<void*> cbTailFuncAddr;
-};
 
 vector<HOOK> gHookList;
 
