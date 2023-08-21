1ba021240699f9d73fb49cba7004b695dc76021f
take <takexaz@gmail.com>
2023-08-21 22:01:24 +0900

    プラグインロードの処理を追加

--- include/_Plugin.hpp
+++ include/_Plugin.hpp
@@ -17,15 +17,20 @@ namespace mebius {
 		explicit PluginsLoader(const std::string& ext) : _extension(ext), _path(fs::current_path().string() + "\\mods\\") {
 			for (auto& p : fs::recursive_directory_iterator(_path))
 			{
-				if (p.path().extension() == ext) {
-					_files.push_back(p.path().string().c_str());
-				}
+				if (p.path().extension() == ext) _files.push_back(p.path().string().c_str());
 			}
 		}
 		void load(void) {
+			// ファイル名一覧からDLLをロード
 			for (auto&& f : _files) {
-				LoadLibraryA(f);
+				if (HMODULE lib = LoadLibraryA(f)) {
+					// ロードした情報を検索して頭がなければVectorに追加
+					if (std::find(_plugins.begin(), _plugins.end(), lib) == _plugins.end()) {
+						_plugins.push_back(std::move(lib));
+					}
+				}
 			}
+
 		}
 		static void free(void) {
 			for (auto&& h : _plugins) {
