b78498006c4a35ebd2f1b4602403ab126ebb3ee2
take <takexaz@gmail.com>
2023-08-21 16:43:56 +0900

    pluginのメンバ関数名を修正

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -6,9 +6,9 @@ std::vector<HMODULE> PluginsLoader::_plugins;
 
 void mebius::LoadPlugins(const std::string & ext) {
     PluginsLoader loader(ext);
-    loader.loadall();
+    loader.load();
 }
 
 void mebius::FreePlugins() {
-    PluginsLoader::freeall();
+    PluginsLoader::free();
 }
